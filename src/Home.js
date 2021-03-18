166b13b449614486e1f71279e88b16bf711c64fb
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-03-18 11:56:07 -0400

    first commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Card } from 'bootstrap'; 
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>Welcome to my Portfolio</h2>
+      </header>
+
+
+        
+    </div>
+  );
+}
+
+export default Home;
