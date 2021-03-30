59e42f495066db4e00003457a26d1bb9e4d2d246
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-03-30 17:06:12 -0400

    added images of projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import NavCRM from '../../images/navcrm.jpeg'; 
+import PlanetWarrior from '../../images/planetwarrior.png'; 
+import BiteBoard from '../../images/biteboard.jpeg'; 
+import GolfLive from '../../images/golflive.jpeg'; 
+import MemeShare from '../../images/memeshare.jpeg'; 
+import './Projects.css'; 
+
 
 const Projects = () => {
   return (
     <div
       style={{
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '120vh'
+        justifyContent: 'center', 
+        height: '300vh',
+        padding: '20px', 
       }}
     >
-      <h1>Projects</h1>
+      <div className="row">
+        <div className="column">
+          <img src={NavCRM} alt="NavCRM Dashboard" width="600px" />
+        </div>
+        <div className="column">
+          <img src={PlanetWarrior} alt="Planet Warrior App" width="600px" />
+        </div>
+        <div className="column">
+          <img src={BiteBoard} alt="Bite Board Recipe Site" width="600px" />
+        </div>
+        <div className="column">
+          <img src={GolfLive} alt="Golf Live" width="600px" />
+        </div>
+        <div className="column">
+          <img src={MemeShare} alt="Meme Share" width="600px" />
+        </div>
+ 
+      </div>
     </div>
   )
 }
