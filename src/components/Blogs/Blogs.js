a63443142da3d9056ce4796e7d5355a0e954838d
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-05-12 21:22:05 -0400

    adding blog api

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Blogs() {
+
+  const BLOG_URL = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@maryann-navarrete"
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Blogs
